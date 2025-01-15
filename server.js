dea71d56e37c3a61a2916725260af3170ddabdd8
purvika621 <purvikabansal05@gmail.com>
2025-01-15 23:48:14 +0530

    Fix: Added CORS support to backend

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express= require("express");
+var cors=require('cors');
 var app=express();
 var fileuploader=require("express-fileupload");
 var mysql2=require("mysql2");
@@ -20,6 +21,8 @@ cloudinary.config({
 });
 app.use(fileuploader());
 app.use(express.urlencoded(true));
+app.use(express.json()); // For parsing JSON bodies
+app.use(cors()); //enable cors for all routes
 app.use(express.static('public')); // Assuming your pics folder is in the public directory this line is used to make the fixed pics visible in  my website 
 //but due to this line i was not able to open any other html file(only the index.html was opening agian and agian) through the serveer therefore i commented this line
 
